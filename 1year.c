275fba2246f7bf774e611ad50571097809a05db4
Charles Cenizal <dcenizal@ucsc.edu>
2020-01-18 03:15:41 -0800

    created c files

---
+++ 1year.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<assert.h>
+#include<string.h>
+
+
+int main(int argc, char * argv []){
+
+printf("Hello, World!\n");
+
+return EXIT_SUCCESS;
+}
