79f2efbecfbd1b1755c5d1579e98f076b5408d5a
Charles Cenizal <dcenizal@ucsc.edu>
2020-01-18 16:04:46 -0800

    adding linked list implementation

--- 1year.c
+++ 1year.c
@@ -3,8 +3,63 @@
 #include<ctype.h>
 #include<assert.h>
 #include<string.h>
+// private types -----------------
+/*
+typedef struct bibleBook{
+  char * bookName;
+  int * chapters;
+  struct bibleBook* next;
+}bibleBook;
 
+//bible book
+typedef bibleBook* Book;
 
+// newBook()
+// constructor of the Book type
+Book newBook(char*name,int* chaps){
+  Book B = malloc(sizeof(bibleBook));
+  assert(B != NULL);
+  B->bookName = name;
+  B->chapters = chaps;
+  B->next = NULL;
+  return(B);
+}
+
+// Bible Object
+typedef struct BibleObj{
+  Book head;
+  int numItems;
+} BibleObj;
+
+Bible newBible(void){
+  Bible B = malloc(sizeof(BibleObj)){
+    assert(B != NULL);
+    B->head = NULL;
+    B->numItems = 0;
+    return B;
+  }
+}
+
+void insert(Bible B, char *name, int*chapters){
+  // Inserting Genesis
+  if(B->numItems == 0){
+    Book Bib = newBook(name,chapters);
+    Bib->next = B->head;
+    B->head = Bib;
+  }
+  else{
+    Book Bib = newBook(name,chapters);
+    Book position = B->head;
+    while(position->next != NULL){
+      position = position->next;
+    }
+    position->next = Bib;
+  }
+  B->numItems++;
+}
+*/
+
+// Main Method
 int main(int argc, char * argv []){
 
   int versesInBible = 31102;
@@ -12,7 +67,25 @@ int main(int argc, char * argv []){
   int versesPerChapter = 26;
   int chaptersPerDay = versesPerDay/versesPerChapter;
   int temp = 0;
-  printf("You will be reading %d chapters per day\n",chaptersPerDay);
+
+  char * name;
+  char * chapters;
+
+  char * books[] = {"Genesis","Exodus","Leviticus","Numbers","Deuteronomy","Joshua",
+                    "Judges, Ruth","1 Samuel","2 Samuel","1 Kings","2 Kings","1 Chronicles",
+                    "2 Chronicles","Ezra","Nehemiah","Esther","Job","Psalm","Proverbs",
+                    "Ecclesiates","Song of Solomon","Isaiah","Jeremiah","Lamentations",
+                    "Ezekiel","Daniel","Hosea","Joel","Amos","Obadiah","Jonah","Micah",
+                    "Nahum","Habakkuk","Zephaniah","Haggai","Zechariah","Malachi","Matthew",
+                    "Mark", "Luke","John","Acts","Romans","1 Corinthians","2 Corinthians",
+                    "Galatians","Ephesians","Philipians","Colossians","1 Thessalonians",
+                    "2 Thessalonians","1 Timothy","2 Timothy","Titus","Philemon","Hebrews",
+                    "James","1 Peter","2 Peter","1 John","2 John","3 John","Jude","Revelation"
+
+                    }
+
+
+  printf("You will be reading %d chapters per day<br />\n",chaptersPerDay);
   // Loops through the days of the year
   for(int i = 1; i < 366; i++){
       if(i <= 31)
